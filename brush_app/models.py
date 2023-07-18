c44204d6b1043257e0deb9ea85298f300ace9f5e
Stuartpkd <stuartparkerdesign@gmail.com>
2023-07-18 14:38:28 +0000

    Create model for brush posts

--- brush_app/models.py
+++ brush_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="brush_posts")
+    content = models.TextField()
+    brush_image = CloudinaryField('image', default='placeholder')
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='brush_likes', blank=True)
