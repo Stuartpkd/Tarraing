25f5735a4835bc1b8da74d5c9738a68c90df325d
Stuartpkd <stuartparkerdesign@gmail.com>
2023-07-18 16:06:02 +0000

    Update summernote url in admin and url files

--- brush_app/admin.py
+++ brush_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
+
+
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
+
+
+admin.site.register(Post)
 
 # Register your models here.
