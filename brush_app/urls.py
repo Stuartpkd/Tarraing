5545638e914dd3f859ba3c13df6527884c7dcc72
Stuartpkd <stuartparkerdesign@gmail.com>
2023-07-21 12:07:47 +0000

    Update url path names for nav

--- brush_app/urls.py
+++ brush_app/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home')
+    path('', views.PostList.as_view(), name='home'),
+    path('brushes/', views.PostList.as_view(), name='brushes'),
+    path('profile/', views.PostList.as_view(), name='profile'),
+    path('upload/', views.PostList.as_view(), name='upload'),
 ]
