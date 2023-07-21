98c54b18a92c582fcdd2fae85319aea784af431c
Stuartpkd <stuartparkerdesign@gmail.com>
2023-07-21 13:30:09 +0000

    Add code for displaying brush images on posts

--- brush_app/urls.py
+++ brush_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('brushes/', views.PostList.as_view(), name='brushes'),
     path('profile/', views.PostList.as_view(), name='profile'),
     path('upload/', views.PostList.as_view(), name='upload'),
+    path('register/', views.PostList.as_view(), name='register'),
 ]
