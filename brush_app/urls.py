1b9695bbce61f839686e6d5519485ff79647460c
Stuartpkd <stuartparkerdesign@gmail.com>
2023-07-24 13:12:12 +0000

    Add post_detail to urls.py

--- brush_app/urls.py
+++ brush_app/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('brushes/', views.PostList.as_view(), name='brushes'),
     path('profile/', views.PostList.as_view(), name='profile'),
     path('upload/', views.PostList.as_view(), name='upload'),
