65124bcd5f0dbe0402c6b99732e1f87a255e3740
Stuartpkd <stuartparkerdesign@gmail.com>
2023-07-24 12:40:12 +0000

    Import view and get object or 404 to views.py

--- brush_app/views.py
+++ brush_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 
@@ -7,3 +7,6 @@ class PostList(generic.ListView):
     model = Post
     template_name = 'index.html'
     paginate_by = 6
+
+class PostDetail(View):
+    
