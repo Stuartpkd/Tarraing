b754ccb9e5ccea0f09ea6db1365f39f6b6a894ae
Stuartpkd <stuartparkerdesign@gmail.com>
2023-07-25 14:41:04 +0000

    Update nav links for signup and logout

--- tarraing/settings.py
+++ tarraing/settings.py
@@ -52,6 +52,11 @@ INSTALLED_APPS = [
     'brush_app',
 ]
 
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
