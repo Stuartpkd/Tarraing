b43174fd8a79890b442a4c38ae909678f8c407e6
Stuartpkd <stuartparkerdesign@gmail.com>
2023-07-31 14:07:11 +0000

    Update urls for the profile page.

--- brush_app/urls.py
+++ brush_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
     path('upload/', views.Upload.as_view(), name='upload'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('profile/', views.Upload.as_view(), name='profile'),
 ]
