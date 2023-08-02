447f29f51837dec355e9e8b0928811ffffad7418
Stuartpkd <stuartparkerdesign@gmail.com>
2023-08-02 13:08:29 +0000

    Update urls for delete post ability.

--- brush_app/urls.py
+++ brush_app/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('profile/<str:username>/', views.ProfileView.as_view(),
          name='profile'),
     path('post/<slug:slug>/edit/', PostEdit.as_view(), name='edit_post'),
+    path('post/<slug:slug>/delete/', PostDelete.as_view(), name='delete_post'),
     path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
     path('upload/', views.Upload.as_view(), name='upload'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
