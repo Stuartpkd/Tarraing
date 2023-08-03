017d028b110371eb3ef5fcdca723d47db0933c61
Stuartpkd <stuartparkerdesign@gmail.com>
2023-08-03 15:19:45 +0000

    Update urls for search function.

--- brush_app/urls.py
+++ brush_app/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('post/<slug:slug>/delete/', PostDelete.as_view(), name='delete_post'),
     path('comment/<int:comment_id>/edit/', views.CommentEdit.as_view(), name='edit_comment'),
     path('comment/<int:comment_id>/delete/', views.CommentDelete.as_view(), name='delete_comment'),
+    path('search/', views.search_posts, name='search_posts'),
     path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
     path('upload/', views.Upload.as_view(), name='upload'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
