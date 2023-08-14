461f53fae9810868a15232a0e968426b4ca16a50
Stuartpkd <stuartparkerdesign@gmail.com>
2023-08-14 12:01:25 +0000

    Add reported to the admin register fields.

--- brush_app/admin.py
+++ brush_app/admin.py
@@ -16,7 +16,7 @@ class PostAdmin(SummernoteModelAdmin):
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
 
-    list_display = ('name', 'body', 'post', 'created_on',)
+    list_display = ('name', 'body', 'post', 'created_on', 'reported')
     list_filter = ('created_on',)
     search_fields = ('name', 'email', 'body')
 
