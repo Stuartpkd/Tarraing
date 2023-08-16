9d5f629edabe78e24b5ff7896315a694779b224b
Stuartpkd <stuartparkerdesign@gmail.com>
2023-08-16 12:29:20 +0000

    Add responsiveness and styling to login page.

--- static/js/script.js
+++
@@ -1,6 +0,0 @@
-// $(document).ready(function () {
-//     $('.toggle-switch input[type="radio"]').change(function () {
-//         $('.toggle-content').hide();
-//         $('#' + $(this).attr('id') + '_content').show();
-//     });
-// });
