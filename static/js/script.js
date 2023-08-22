1789ae10e3683fe2d957dd17d5a659e8a2d8e65b
Stuartpkd <stuartparkerdesign@gmail.com>
2023-08-22 10:53:04 +0000

    Fix error with message timeout function.

--- static/js/script.js
+++ static/js/script.js
@@ -1,33 +1,35 @@
-let savedArtworkBtn = document.getElementById("artwork-btn"); // button to toggle saved artworks
+document.addEventListener("DOMContentLoaded", function () {
+    let savedArtworkBtn = document.getElementById("artwork-btn");
+    let postsBtn = document.getElementById("post-btn");
+    let posts = document.getElementById("post-list");
+    let savedArtworks = document.getElementById("saved-artwork-list");
 
-let postsBtn = document.getElementById("post-btn"); // button to toggle to see posts instead
+    savedArtworkBtn.addEventListener("click", displayArtworks);
+    postsBtn.addEventListener("click", displayPosts);
 
-let posts = document.getElementById("post-list"); // to show the posts
-
-let savedArtworks = document.getElementById("saved-artwork-list"); // to show the artworks
-
-savedArtworkBtn.addEventListener("click", displayArtworks);
-postsBtn.addEventListener("click", displayPosts);
-
-function displayArtworks() {
-    if (savedArtworks.classList.contains('hide')) {
-        savedArtworks.classList.remove("hide");
-        posts.classList.add('hide');
-    } else {
-
+    function displayArtworks() {
+        if (savedArtworks.classList.contains('hide')) {
+            savedArtworks.classList.remove("hide");
+            posts.classList.add('hide');
+        }
     }
-}
 
-function displayPosts() {
-    if (posts.classList.contains('hide')) {
-        posts.classList.remove('hide');
-        savedArtworks.classList.add('hide');
-    } else {
-
+    function displayPosts() {
+        if (posts.classList.contains('hide')) {
+            posts.classList.remove('hide');
+            savedArtworks.classList.add('hide');
+        }
     }
-}
+});
 
 
 function confirmDelete() {
     return confirm("Are you sure you want to delete this post?");
 }
+
+
+setTimeout(function () {
+    let messages = document.getElementById('msg');
+    let alert = new bootstrap.Alert(messages);
+    alert.close();
+}, 2500);
